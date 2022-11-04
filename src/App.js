2ddba7153eb5af576b9f8e4227b08279b207250d
China Jameson <csjameson008@gmail.com>
2022-11-04 14:47:20 -0400

    pages added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Navbar from './components/index.js';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Blogs from './pages/blogs';
+import SignUp from './pages/sign-up';
+import Contact from './pages/contact';
+  
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+    <Router>
+    <Navbar />
+    <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/about' element={<About/>} />
+        <Route path='contact' element={<Contact/>} />
+        <Route path='/blogs' element={<Blogs/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+    </Routes>
+    </Router>
+);
 }
+  
+
+// const Header = lazy(() => import('./components/Header'));
+// const Hero = lazy(() => import('./components/Hero'));
+// const Project = lazy(() => import('./components/Project'));
+// const Skill = lazy(() => import('./components/Skill'));
+// const Contact = lazy(() => import('./components/Contact'));
+// const Experience = lazy(() => import('./components/Experience'))
+// function App() {
+//   return (
+//     <Suspense
+//       fallback={
+//         <div className='sk-folding-cube'>
+//           <div className='sk-cube1 sk-cube'></div>
+//           <div className='sk-cube2 sk-cube'></div>
+//           <div className='sk-cube4 sk-cube'></div>
+//           <div className='sk-cube3 sk-cube'></div>
+//         </div>
+//       }
+//     >
+//       <div className='App container my-10 mx-auto max-w-screen-lg bg-black'>
+//         <Header />
+//         <main>
+//           <Hero />
+//           <Experience />
+//           <Project />
+//           <Skill />
+//           <Contact />
+//         </main>
+//       </div>
+//     </Suspense>
+//   );
+// }
+
+// function App() {
+//   return (
+//       <Router>
+//       <Navbar />
+//       <Routes>
+//           <Route exact path='/' exact element={<Hero />} />
+//           <Route path='/Project' element={<About/>} />
+//           <Route path='/Experience' element={<Experience/>} />
+//           <Route path='/Skill' element={<Skill/>} />
+//           <Route path='/Contact' element={<Contact/>} />
+//       </Routes>
+//       </Router>
+//   );
+//   }
+
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import { Nav, NavLink, NavMenu } 
+//     from "./components/NavbarElements";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +13,48 @@ root.render(
   </React.StrictMode>
 );
 
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//For app to work offline
+// serviceWorker.register();
+// function App() {
+//   return (
+//       <Router>
+//       <Navbar />
+//       <Routes>
+//           <Route exact path='/'  element={<Hero />} />
+//           <Route path='/Project' element={<About/>} />
+//           <Route path='/Experience' element={<Experience/>} />
+//           <Route path='/Skill' element={<Skill/>} />
+//           <Route path='/Contact' element={<Contact/>} />
+//       </Routes>
+//       </Router>
+//   );
+//   }
+// const Navbar = () => {
+//   return (
+//     <>
+//       <Nav>
+//         <NavMenu>
+//           <NavLink to="/Project" activeStyle>
+//             Project
+//           </NavLink>
+//           <NavLink to="/Experience" activeStyle>
+//             Experience
+//           </NavLink>
+//           <NavLink to="/Skill" activeStyle>
+//             Skill
+//           </NavLink>
+//           <NavLink to="/Contact" activeStyle>
+//             Contact
+//           </NavLink>
+//         </NavMenu>
+//       </Nav>
+//     </>
+//   );
+// };
+  // export default Navbar;
